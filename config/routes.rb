19ca404af99ac4b1336901b4da0042d78574e088
Sofiane Gargouri <contact@sofianeg.com>
2018-05-31 15:06:07 +0200

    :rocket: Adding UserResource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :user_resources
   resources :parts
   resources :resources
   resources :stats
--- db/migrate/20180531124015_create_rockets.rb
+++ db/migrate/20180531124015_create_rockets.rb
@@ -6,7 +6,7 @@ class CreateRockets < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :image_url
 
-      t.belongs_to :user, type: :uuid
+      t.belongs_to :user, foreign_key: true, type: :uuid
 
       t.timestamps
     end
