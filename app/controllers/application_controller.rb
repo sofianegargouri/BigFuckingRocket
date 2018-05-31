1d87dbfdcf47e4bce97b5962205c53348351fb59
SofianeG <contact@sofianeg.com>
2018-05-31 16:27:27 +0200

    :rocket: Models update (#3)
    * :rocket: Adding level up
    
    * :rocket: Fixing Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :update_user_resources
   protect_from_forgery with: :exception
+
+  def update_user_resources
+    current_user&.update_resources
+  end
 end
---
+++ app/controllers/rocket_parts_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class RocketPartsController < ApplicationController
+  load_and_authorize_resource
+
+  # GET /rocket_parts
+  # GET /rocket_parts.json
+  def level_up
+    @rocket_part.level_up
+  end
+end
