76529ecd4d88246e1492f379c3c96cde409c9994
Sofiane Gargouri <contact@sofianeg.com>
2018-06-04 10:37:47 +0200

    :rocket: Updating Rocket to SVG

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -3,47 +3,15 @@
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).ready(function() {
-
-  function isDisplayed(id) {
-    var isDisplayed = $('#'+id).css('display')
-    if(isDisplayed === "flex")
-      return true
-    return false
-  }
-
-  function display(id) {
-    $('#'+id).css('display', 'flex')
-  }
-
-  function hide(id) {
-    $('#'+id).css('display', 'none')
-  }
-
-  $('#upgrade-btn-fins').on('click', function() {
-    if(isDisplayed("upgrade-panel-fins"))
-      hide("upgrade-panel-fins")
-    else
-      display("upgrade-panel-fins")
-  })
-
-  $('#upgrade-btn-tube').on('click', function() {
-    if(isDisplayed("upgrade-panel-tube"))
-      hide("upgrade-panel-tube")
-    else
-      display("upgrade-panel-tube")
-  })
-
-  $('#upgrade-btn-nose').on('click', function() {
-    if(isDisplayed("upgrade-panel-nose"))
-      hide("upgrade-panel-nose")
-    else
-      display("upgrade-panel-nose")
-  })
-
-  $('#upgrade-btn-hollow-pressure-vessel').on('click', function() {
-    if(isDisplayed("upgrade-panel-hollow-pressure-vessel"))
-      hide("upgrade-panel-hollow-pressure-vessel")
-    else
-      display("upgrade-panel-hollow-pressure-vessel")
+  $('.upgrade-button').on('click', function() {
+    var id = $(this).attr('id')
+    var position = $(this).position()
+    $('.upgrade-panel').hide()
+    $('#upgrade-panel-' + id).css({
+      display: 'flex',
+      left: position.left + $(this)[0].getBoundingClientRect().width,
+      position: 'fixed',
+      top: position.top + $(this)[0].getBoundingClientRect().height,
+    })
   })
 })
